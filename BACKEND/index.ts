b6060c64ba4fb47b422bdb768912459c2580a63d
judithmg <jmartigo33@gmail.com>
2021-03-06 22:28:58 +0100

    Configured eslint for TS at backend

---
+++ BACKEND/index.ts
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const { connect } = require('mongoose');
+
+const model = require('./src/models/model');
+
+const route = require('./src/routes/route');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+connect('mongodb+srv://judith:judith@cluster0.sloat.mongodb.net/pokehub', { useNewUrlParser: true, useUnifiedTopology: true });
+const hello = [];
+
+app.use(morgan('dev'));
+
+app.use(express.urlencoded({ extend: true }));
+app.use(express.json());
+
+app.use('/pokehub', route);
+
+app.listen(port, () => {
+  debug(`PROMOFARMA is running in ${chalk.bgGreen.bold(`http://localhost:${port}`)}`);
+});
