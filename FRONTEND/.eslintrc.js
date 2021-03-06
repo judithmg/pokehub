cb777246d093e288b9320cd3a4e43f3781bfdc9e
judithmg <jmartigo33@gmail.com>
2021-03-06 19:47:07 +0100

    Created files for tests

--- FRONTEND/.eslintrc.js
+++ FRONTEND/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
   extends: [
     'plugin:react/recommended',
@@ -21,5 +22,7 @@ module.exports = {
     'linebreak-style': 0,
     'global-require': 0,
     'eslint linebreak-style': [0, 'error', 'windows'],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+
   },
 };
