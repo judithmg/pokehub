d99cbeeea4469347c0a24aa71c21603e3b7d6076
judithmg <jmartigo33@gmail.com>
2021-03-06 20:30:58 +0100

    Working on header

--- FRONTEND/src/pages/landing/index.jsx
+++ FRONTEND/src/pages/landing/index.jsx
@@ -4,6 +4,7 @@ export default function LandingComponent() {
   return (
     <>
       <img
+        className="main__logo"
         src="https://trello-attachments.s3.amazonaws.com/6041f83090af1242e84592ce/922x400/955ef6e7ad86fde9fc7ef8772a7f7596/pokeshelfdrib.png"
         alt="Header logo"
       />
