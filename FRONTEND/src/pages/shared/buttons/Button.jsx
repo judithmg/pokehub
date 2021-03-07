a3a3f75d29c221a3aba95fa0d3074e13b1844db8
judithmg <jmartigo33@gmail.com>
2021-03-07 10:45:24 +0100

    Styles for button

--- FRONTEND/src/pages/shared/buttons/Button.jsx
+++ FRONTEND/src/pages/shared/buttons/Button.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import '../../../styles/button.scss';
 
-export default function ButtonComponent({ text }) {
+export default function ButtonComponent({ text, classes }) {
   return (
-    <button type="button">{text}</button>
+    <button type="button" className={`${classes} btn`}>{text}</button>
   );
 }
+
+ButtonComponent.propTypes = {
+  text: PropTypes.string.isRequired,
+  classes: PropTypes.string.isRequired,
+};
