9da6a0c34884437573440988f45eb81a6e5f2707
judithmg <jmartigo33@gmail.com>
2021-03-07 11:57:36 +0100

    Created React components for all icons

---
+++ FRONTEND/src/assets/icons/index.js
@@ -0,0 +1,4 @@
+export { default as Avatar } from './Avatar';
+export { default as Pokeball } from './Pokeball';
+export { default as PokemonIrPik } from './PokemonIrPik';
+export { default as PokemonIr } from './PokemonIr';
