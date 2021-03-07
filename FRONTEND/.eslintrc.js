fce8648ec983b5161d5354467baea9fea887abfd
judithmg <jmartigo33@gmail.com>
2021-03-07 11:59:56 +0100

    Added Eslint rule to override error with props spreading Package.json updated to add svg library

--- FRONTEND/.eslintrc.js
+++ FRONTEND/.eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     'global-require': 0,
     'eslint linebreak-style': [0, 'error', 'windows'],
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-props-no-spreading': 'off',
 
   },
 };
