d9df17bbdd9c92c24320f8006492283bc6fa3894
judithmg <jmartigo33@gmail.com>
2021-03-07 10:59:04 +0100

    Class for desktop elements Button element functional Login button added to header Desktop class added to main logo

--- FRONTEND/src/pages/main/index.jsx
+++ FRONTEND/src/pages/main/index.jsx
@@ -5,7 +5,7 @@ export default function MainComponent() {
   return (
     <main>
       <img
-        className="main__logo"
+        className="main__logo --desktop"
         src="https://trello-attachments.s3.amazonaws.com/6041f83090af1242e84592ce/922x400/955ef6e7ad86fde9fc7ef8772a7f7596/pokeshelfdrib.png"
         alt="Header logo"
       />
--- FRONTEND/src/pages/shared/header/index.jsx
+++ FRONTEND/src/pages/shared/header/index.jsx
@@ -3,21 +3,28 @@
 import React, { useState } from 'react';
 import '../../../styles/header.scss';
 import Menu from '../menu';
+import Button from '../buttons/Button';
 
 export default function LandingComponent() {
   const [menu, setMenu] = useState(false);
 
   return (
+    <>
+      <header>
 
-    <header>
-      <span>*logo goes here</span>
-      <img
-        src="https://trello-attachments.s3.amazonaws.com/6041f83090af1242e84592ce/100x100/4f2a7611af73115d6ef54a5815b846a5/dayo3ow-7ac86c31-8b2b-4810-89f2-e6134caf1f2d.gif"
-        alt="pokeball"
-        className="header__pokeball"
-        onClick={() => setMenu(!menu)}
-      />
+        <span className="header__logo">*logo goes here</span>
+        <div className="header--right">
+          <Button text="Login" classes="header__login" />
+          <img
+            src="https://trello-attachments.s3.amazonaws.com/6041f83090af1242e84592ce/100x100/4f2a7611af73115d6ef54a5815b846a5/dayo3ow-7ac86c31-8b2b-4810-89f2-e6134caf1f2d.gif"
+            alt="pokeball"
+            className="header__pokeball"
+            onClick={() => setMenu(!menu)}
+          />
+
+        </div>
+      </header>
       {menu && (<Menu />)}
-    </header>
+    </>
   );
 }
