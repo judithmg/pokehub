a1d2997455726fe841270f5b0e09c7534f7859f9
judithmg <jmartigo33@gmail.com>
2021-03-07 12:45:04 +0100

    Dashboard component test passing

--- FRONTEND/src/__tests__/Dashboard.test.js
+++ FRONTEND/src/__tests__/Dashboard.test.js
@@ -1,3 +1,18 @@
-test('Then there should be a button element', () => {
-  expect(true).toBe(true);
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import DashboardElement from '../pages/dashboard';
+
+describe('Given a Dashboard component', () => {
+  describe('When it is invoked', () => {
+    test('Then there should be a section', () => {
+      render(
+        <DashboardElement />,
+      );
+
+      const dashboard = screen.findByLabelText('section');
+
+      expect(dashboard).toBeTruthy();
+    });
+  });
 });
