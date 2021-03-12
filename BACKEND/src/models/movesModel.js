d0ef0c05adc5ddc858f86eca1ab96270ac8be5b7
judithmg <jmartigo33@gmail.com>
2021-03-12 10:42:45 +0100

    Modified models Schema

--- BACKEND/src/models/movesModel.js
+++ BACKEND/src/models/movesModel.js
@@ -12,6 +12,42 @@ const movesSchema = new Schema({
     type: String,
     required: true,
   },
+  accuracy: {
+    type: [],
+  },
+  basePower: {
+    type: Number,
+  },
+  category: {
+    type: String,
+  },
+  id: {
+    type: String,
+    required: true,
+
+  },
+  pp: {
+    type: Number,
+  },
+  num: {
+    type: Number,
+  },
+  priority: {
+    type: Number,
+  },
+  critRatio: {
+    type: Number,
+  },
+  target: {
+    type: String,
+  },
+  type: {
+    type: String,
+  },
+  contestType: {
+    type: String,
+  },
+
 });
 
 module.exports = model('Move', movesSchema);
