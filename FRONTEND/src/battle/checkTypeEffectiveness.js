3e8e3ab7becce5dc056ddc7b307e9f8447ad3e1a
judithmg <jmartigo33@gmail.com>
2021-03-23 12:39:42 +0100

    Package json

--- FRONTEND/src/battle/checkTypeEffectiveness.js
+++
@@ -1,8 +0,0 @@
-export default function checkTypeEffectiveness(attacker, deffendant, typechart) {
-  const typeAttack = attacker.attack.type;
-  const typeDeffendant = deffendant.types;
-
-  const attackerEffectiveness = typechart.filter((type) => type.toLowerCase() === typeAttack);
-
-  typeDeffendant.map((type) => attackerEffectiveness[type]);
-}
