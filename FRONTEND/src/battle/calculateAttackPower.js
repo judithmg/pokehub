8b9d35c4004493ec7f9681d1e023492b8101327a
judithmg <jmartigo33@gmail.com>
2021-03-24 10:11:10 +0100

    fixed eslint rules

--- FRONTEND/src/battle/calculateAttackPower.js
+++ FRONTEND/src/battle/calculateAttackPower.js
@@ -1,17 +1,13 @@
-/* eslint-disable no-debugger */
-/* eslint-disable no-mixed-operators */
-/* eslint-disable max-len */
-
 export default function calculateAttackPower(attackData, attacker, defendant, modifier) {
   if (attackData.category.toUpperCase() === 'SPECIAL') {
     return (Math.floor(((((2 * attacker.level / 5) + 2)
-     * attackData.basePower * (attacker.battleStats.spa / defendant.battleStats.spd) / 50) + 2)) * modifier);
+     * attackData
+       .basePower * (attacker.battleStats.spa / defendant.battleStats.spd) / 50) + 2)) * modifier);
   }
 
   if (attackData.category.toUpperCase() === 'PHYSICAL') {
-    return (Math.floor(((((2 * attacker.level / 5) + 2) * attackData.basePower * (attacker.battleStats.atk / defendant.battleStats.def) / 50) + 2)) * modifier);
+    return (Math.floor(((((2 * attacker.level / 5) + 2) * attackData
+      .basePower * (attacker.battleStats.atk / defendant.battleStats.def) / 50) + 2)) * modifier);
   }
   return 0;
 }
-
-// special physical constantes
